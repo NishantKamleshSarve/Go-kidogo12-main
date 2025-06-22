aac16ea6a9fca542402154f812c60d926954b516
niks <nishantsarve73@mgmail.com>
2025-06-23 01:33:07 +0530

    changes

--- frontend/Go-kidogo12-main/src/pages/RestaurantMenu.jsx
+++ frontend/Go-kidogo12-main/src/pages/RestaurantMenu.jsx
@@ -17,7 +17,7 @@ const RestaurantMenu = () => {
   useEffect(() => {
     const fetchMenu = async () => {
       try {
-        const response = await fetch('https://go-kidogo.onrender.com/api/restaurentdetail', {
+        const response = await fetch('https://go-kidogo12-main.onrender.com/api/restaurentdetail', {
           method: 'POST',
           headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify({
